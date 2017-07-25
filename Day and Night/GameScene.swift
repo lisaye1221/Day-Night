630adac317f8de6db02431458a2fca0174b2d197
Lisa Ye <lisaye1999@gmail.com>
2017-07-25 00:03:32 -0400

    implement jump button
    jump impluse of 28
    added limited variable: playerOnGround to ensure the player don't spam the jump button, causing the egg to fly off

--- Day and Night/GameScene.swift
+++ Day and Night/GameScene.swift
@@ -1,3 +1,4 @@
+
 //
 //  GameScene.swift
 //  Day and Night
@@ -9,26 +10,60 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
     
     let fixedDelta: CFTimeInterval = 1.0 / 60.0
     var score: CFTimeInterval = 0
     
+    var egg: SKSpriteNode!
+    var eggReference: SKReferenceNode!
     var scoreLabel: SKLabelNode!
+    var jumpButton: MSButtonNode!
+    
+    var playerOnGround: Bool = true
+    
+    
     
     override func didMove(to view: SKView) {
         
+        egg = childNode(withName: "//egg") as! SKSpriteNode
+        eggReference = childNode(withName: "eggReference") as! SKReferenceNode
         scoreLabel = childNode(withName: "scoreLabel") as! SKLabelNode
-      
-    }
+        jumpButton = childNode(withName: "jumpButton") as! MSButtonNode
+        
+        physicsWorld.contactDelegate = self
+        
+        
+        jumpButton.selectedHandler = {
+            if self.playerOnGround {
+                self.egg.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 28))
+                self.playerOnGround = false
+            }
+        }
+        
+        
+        
+    }//closing brackets for didMove function
+    
     
     
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-  
         
+        
+        
+    }
+    
+    
+    
+    func didBegin(_ contact: SKPhysicsContact) {
+        if contact.bodyA.categoryBitMask == 1 && contact.bodyB.categoryBitMask == 2 ||
+            contact.bodyA.categoryBitMask == 2 && contact.bodyB.categoryBitMask == 1 {
+            //contact.bodyB.node?.removeFromParent()  //removes the node that is bodyB
+            playerOnGround = true
         }
         
+    }
     
     
     override func update(_ currentTime: TimeInterval) {
@@ -37,7 +72,16 @@ class GameScene: SKScene {
         score += fixedDelta
         scoreLabel.text = "\(Int(score))"
         
+        //        /* Grab player's vertical velocity */
+        //        let velocityY = egg.physicsBody?.velocity.dy ?? 0
+        //
+        //        /* Check and cap vertical velocity */
+        //        if velocityY > 25 {
+        //            egg.physicsBody?.velocity.dy = 25
+        //
+        //        }
+        
     }
-        
-        
-}//Closing brackets
+    
+    
+}//Closing brackets for the gamescene class
